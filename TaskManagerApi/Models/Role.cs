6aae751c1e26d8f7cab336a03da540cc87bb3f55
Abdullah Imran <bcsf24a017@pucit.edu.pk>
2026-08-13 14:50:45 +0500

    Updated Models

--- TaskManagerApi/Models/Role.cs
+++ TaskManagerApi/Models/Role.cs
@@ -1,9 +1,12 @@
 
+using System.Text.Json.Serialization;
+
 namespace TaskManager.Api.Models;
 
 public class Role
 {
     public int Id {get; set;}
     public string Name {get; set;} = string.Empty;
+    [JsonIgnore]
     public ICollection<User> Users{get; set;} = new List<User>();
 }
--- TaskManagerApi/Models/User.cs
+++ TaskManagerApi/Models/User.cs
@@ -6,6 +6,7 @@ public class User
     public int Id { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
+      public string Email { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
     public int RoleId { get; set; }
     public bool isActive { get; set; } = true;
