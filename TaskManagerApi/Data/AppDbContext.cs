d29023e7765a501d406d30e8e11581b531343701
Abdullah Imran <bcsf24a017@pucit.edu.pk>
2026-08-13 12:20:19 +0500

    Adding our user and role models to our AppDbContext

--- TaskManagerApi/Data/AppDbContext.cs
+++ TaskManagerApi/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.EntityFrameworkCore;
+using TaskManager.Api.Models;
 
 namespace TaskManager.Api.Data;
 
@@ -8,6 +9,8 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options)
     : base(options)
     {
-        
     }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+
 }
